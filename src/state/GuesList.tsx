5f413e547f3d3c6f23cf8218a3d58efe9c5d50cf
rigole <foplacide@gmail.com>
2022-09-11 10:35:17 +0100

    state and typeScript 2

--- src/state/GuesList.tsx
+++ src/state/GuesList.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { useState } from "react";
 
+
+
 const GuestList: React.FC = () => {
     const [name, setName] = useState("")
+    const [guests, setGuests] = useState<string[]>([])
+
+
+    const addGuest = () => {
+        setName('');
+        setGuests([...guests, name]);
+    }
     return (
         <div>
             <h2>Guest List</h2>
+            <ul>
+                {guests.map((guest) => (
+                    <li key={guest}>{guest}</li>
+                ))}
+            </ul>
             <input value={name} onChange={(e) => setName(e.target.value)}/>
-            <button>add a Guest</button>
+            <button onClick={addGuest}>add a Guest</button>
         </div>
     )
 }
