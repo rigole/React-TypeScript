b3d8046a1300f7812838a565b70e73cb43bc1d40
rigole <foplacide@gmail.com>
2022-09-16 16:41:36 +0100

    ref & class component

---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,37 @@
+import  React, {useState, useRef} from "react";
+
+const users = [
+    { name: 'Mary', age: 30 },
+    { name: 'Michael', age: 50 },
+    { name: "Didier", age: 15 }
+]
+
+
+
+
+const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [name, setName] = useState('');
+    const [user, setUser] = useState<{ name: string, age: number } | undefined>();
+
+    const onClick = () => {
+        const foundUser = users.find((user) => {
+            return user.name === name;
+        });
+
+        setUser(foundUser);
+    }
+    return (
+        <div>
+            User Search
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}  type="text" />
+            <button onClick={onClick}>Find User</button>
+            <div>
+                {user && user.name}
+                {user && user.age}
+            </div>
+        </div>
+    )
+}
+
+export default UserSearch;
